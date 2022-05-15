a9a2689b187106108d53993ecd9a81eb06f7dcbf
nurulsyfiqah <nurulsyfiqah25@gmail.com>
2022-05-15 23:42:15 +0800

    Adding Sequential Part

---
+++ src/ifarm/data/Activity.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifarm.data;
+
+/**
+ *
+ * @author user
+ */
+public class Activity {
+    
+    private String id;
+    private String date;
+    private String action;
+    private String type;
+    private String unit;
+    private String quantity;
+    private String field;
+    private String row;
+    private String farmId;
+    private String userId;
+
+    public Activity() {
+    }
+    
+    public Activity(String id, String date, String action, String type, String unit, String quantity, String field, String row, String farmId, String userId) {
+        this.id = id;
+        this.date = date;
+        this.action = action;
+        this.type = type;
+        this.unit = unit;
+        this.quantity = quantity;
+        this.field = field;
+        this.row = row;
+        this.farmId = farmId;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public String getFarmId() {
+        return farmId;
+    }
+
+    public void setFarmId(String farmId) {
+        this.farmId = farmId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+    
+    
+}
