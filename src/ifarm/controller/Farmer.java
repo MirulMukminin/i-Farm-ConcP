92af0951743a0ecf82cbb8fabf027e879589cfba
Hanani <nurulhnani@gmail.com>
2022-06-04 02:08:15 +0800

    Adding Concurrent part

--- src/ifarm/controller/Farmer.java
+++ src/ifarm/controller/Farmer.java
@@ -1,6 +1,9 @@
 package ifarm.controller;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,45 +11,46 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Farmer implements Runnable {
-ActivityLog log;
-private int index;
+public class Farmer implements Runnable{
+LogAccess farmerLog;
+private int numOfActivities;
+HashMap<String, String> map = new HashMap<String, String>();
 
-    public Farmer(ActivityLog log){
-        this.log = log;
+    public Farmer(LogAccess farmerLog, int numOfActivities) {
+        this.farmerLog = farmerLog;
+        this.numOfActivities = numOfActivities;
     }
     
     @Override
     public void run() {
+    try {
+        Random rand = new Random();
+        Utility util = new Utility();
+        String users = util.readFile("farmer.txt");
+        JSONArray userArr = new JSONArray(users);
+        JSONObject userObj = null;
+        String userID = "";
+        List<String> userFarm = new ArrayList<>();
+
+        //get farmer id and farm object
+        if (userArr.length() != 0) {
+            userObj = userArr.getJSONObject(rand.nextInt(userArr.length()));
+            userID = userObj.getString("id");
+            userFarm = util.stringToArray(userObj.getString("farms"));
+        }    
         
-            try {
-            Random rand = new Random();
-            Utility util = new Utility();
-            String users = util.readFile("farmer.txt");
-            JSONArray userArr = new JSONArray(users);
-            JSONObject userObj = null;
-            String userID = "";
-
-            if (userArr != null) {
-                userObj = userArr.getJSONObject(rand.nextInt(userArr.length()));
-                userID = userObj.getString("id");
-                //System.out.println(Thread.currentThread().getName() + " " + userObj.getString("name"));
-            }
-
-            // generate activities
-            for (int i = 0; i < 1; i++) {
-                int numOfActivities = rand.nextInt(10) + 1000;
-                index = log.generateActivities(userID, index, numOfActivities, log);
-                index++;
-                //System.out.println(Thread.currentThread().getName() + " " + numOfActivities + " with index DB "+indexDb);
-            }
-             
-
-        } catch (FileNotFoundException | JSONException e) {
-            e.printStackTrace();
-        } catch (InterruptedException ex) {
+        int indexLog = 1;
+        for(int i = 0; i<numOfActivities; i++){
+            farmerLog = new LogAccess(userID, userFarm, indexLog, map);
+            farmerLog.generateRandActivities();  
+            indexLog++;
+        }
+        
+        
+    } catch (JSONException | FileNotFoundException ex) {
         Logger.getLogger(Farmer.class.getName()).log(Level.SEVERE, null, ex);
     }
+        
         
     }
     
