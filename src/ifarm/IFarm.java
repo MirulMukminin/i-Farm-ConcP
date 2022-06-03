c62fd5dfb469beb92a6a1ed4554323c1206e8e44
Hanani <nurulhnani@gmail.com>
2022-06-04 02:08:35 +0800

    Adding Concurrent part

--- src/ifarm/IFarm.java
+++ src/ifarm/IFarm.java
@@ -1,18 +1,15 @@
 package ifarm;
 
-import ifarm.controller.ActivityLog;
 import ifarm.controller.Farmer;
 import ifarm.controller.FarmerSeq;
 import ifarm.controller.FarmerSimulator;
+import ifarm.controller.LogAccess;
 import ifarm.data.Farmers;
 import ifarm.dataAccess.activityDA;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import org.json.JSONException;
 
 public class IFarm {
@@ -37,7 +34,8 @@ public class IFarm {
             
             // generate random number of farmers, x > 100
             Random rand = new Random();
-            int numOfFarmers = rand.nextInt(10) + 10;
+            //int numOfFarmers = rand.nextInt(10) + 10;
+            int numOfFarmers = 2;
             System.out.printf("Number of Farmers: %d \n", numOfFarmers);
             // generate random number of activities for each farmers, x > 1000
             int[] numOfActivities = new int[numOfFarmers];
@@ -56,18 +54,17 @@ public class IFarm {
             plantStatus = farmerSimulator.generatePlantFile();
 
             // generate farmers and activities sequentially
-            FarmerSeq fs = new FarmerSeq();
-            fs.generateFarmersActivitiesSeq(numOfFarmers, numOfActivities);
+//            FarmerSeq fs = new FarmerSeq();
+//            fs.generateFarmersActivitiesSeq(numOfFarmers, numOfActivities);
         
             // generate farmers and activities concurrently
-//            ActivityLog actlog = new ActivityLog();
-//            ExecutorService pool = Executors.newFixedThreadPool(numOfFarmers);
-//                for(int i = 0; i<numOfFarmers; i++){
-//                   pool.execute(new Farmer(actlog));
-//                }
-//
-//            pool.shutdown();
-//            pool.awaitTermination(5, TimeUnit.SECONDS);
+            LogAccess farmerLog = new LogAccess();
+            ExecutorService pool = Executors.newFixedThreadPool(numOfFarmers);
+                for(int i = 0; i<numOfFarmers; i++){
+                   pool.execute(new Farmer(farmerLog, numOfActivities[i]));
+                }
+
+            pool.shutdown();
 
         } catch (SQLException | JSONException e) {
         }
