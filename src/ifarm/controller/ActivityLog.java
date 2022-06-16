0b8c95cacc790e9b88e0da63fc3cc637ec5ac05d
nurulhnani <80149026+nurulhnani@users.noreply.github.com>
2022-06-17 04:11:49 +0800

    Update ActivityLog.java

--- src/ifarm/controller/ActivityLog.java
+++ src/ifarm/controller/ActivityLog.java
@@ -60,7 +60,7 @@ private int indexDb;
         return index;
     }
     
-    public int generateActivities(String userID, int index, List<String> userFarms) throws FileNotFoundException, InterruptedException {
+   /* public int generateActivities(String userID, int index, List<String> userFarms) throws FileNotFoundException, InterruptedException {
     lock.lock();
         try {
             // generate activities
@@ -150,7 +150,45 @@ private String userID;
 
     ReentrantLock lock = new ReentrantLock();
     
-        
+     //for disaster simulation
+    public int generateActivities1(String userID, int index, String farmID, JSONObject farmObj) throws FileNotFoundException, InterruptedException {
+    lock.lock();
+        try {
+            // generate activities
+            Utility util = new Utility();
+            Random rand = new Random();
+            // get the farm belong to the farmer
+            // get farms from farmer
+                         
+            
+            GenerateActivity randAct = new GenerateActivity(farmID, farmObj);
+            String date = randAct.getDate();
+            String action = randAct.getAction();
+            String type = randAct.getType();
+            String quantity = randAct.getQuantity();
+            String unit = randAct.getUnit();
+            String field = randAct.getField();
+            String row = randAct.getRow();
+                
+            //Write to database
+            Activity act = new Activity(String.valueOf(indexDb), date, action, type, unit, quantity, field, row, farmID, userID);
+            activityDA actDA = new dbConnection().getActivityDA();
+            actDA.addActivities(act);
+
+            //Write to log
+            writeLog(Thread.currentThread().getName() + " " + index +" Success: " + date + " " + action + " " + type + " successfully inserted");
+            System.out.println(Thread.currentThread().getName() + " " +index + " - " + date + " " + action + " " + type + " " + unit + " " + quantity + " " + field + " " + row + " " + farmID + " " + userID);
+
+            // increment indexDb
+            indexDb++;
+            // increment index for thread
+        } catch (SQLException ex) {
+            Logger.getLogger(ActivityLog.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            lock.unlock();
+        }
+        return index;
+    }   
     public int generateActivities(String userID, int index, List<String> userFarms) throws FileNotFoundException, InterruptedException {
         try {
             this.userID = userID;
